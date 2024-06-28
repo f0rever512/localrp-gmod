cbe2a853213c4a21e81d68e5595ccbc345f58056
Nsfw <75947980+NsfwSZS@users.noreply.github.com>
2024-06-28 21:34:59 +0400

    📦The code is convenient

--- lrp-scoreboard/lua/autorun/client/lrp_derma.lua
+++ lrp-scoreboard/lua/autorun/client/lrp_derma.lua
@@ -1,67 +1,121 @@
+local function addMenuOption(menu, text, icon, func)
+    local option = menu:AddOption(text, func)
+    option:SetIcon(icon)
+    return option
+end
+
+local function addSubMenu(menu, text, icon)
+    local subMenu, subMenuIcon = menu:AddSubMenu(text)
+    subMenuIcon:SetIcon(icon)
+    return subMenu
+end
+
 function LRPDerma(pl)
-	local lrpDerma = DermaMenu()
+    local lrpDerma = DermaMenu()
 
-	lrpDerma:AddOption( pl:GetName() .. " (" .. pl:GetUserGroup() .. ")", function()
-		if IsValid(pl) then
-			pl:ShowProfile()
-		end
-	end):SetIcon( "icon16/user.png" )
-		
-	local steamid = lrpDerma:AddOption( "Скопировать SteamID", function() SetClipboardText(pl:SteamID()) end )
-	steamid:SetIcon( "icon16/information.png" )
+    addMenuOption(lrpDerma, pl:GetName() .. " (" .. pl:GetUserGroup() .. ")", "icon16/user.png", function()
+        if IsValid(pl) then
+            pl:ShowProfile()
+        end
+    end)
 
-	if LocalPlayer():IsAdmin() then
-		lrpDerma:AddSpacer()
+    addMenuOption(lrpDerma, "Скопировать SteamID", "icon16/information.png", function() 
+        SetClipboardText(pl:SteamID()) 
+    end)
 
-		local KickUser = lrpDerma:AddOption( "Кикнуть", function() net.Start( "kickuser" ) net.WriteEntity(pl) net.SendToServer() end )
-		KickUser:SetIcon( "icon16/user_delete.png" )
-		
-		local BanUser, icon = lrpDerma:AddSubMenu("Забанить")
-		icon:SetIcon( "icon16/delete.png" )
-		BanUser:AddOption( "5 Минут", function() net.Start( "5m" ) net.WriteEntity(pl) net.SendToServer() end ):SetIcon( "icon16/time.png" )
-		BanUser:AddOption( "15 Минут", function() net.Start( "15m" ) net.WriteEntity(pl) net.SendToServer() end ):SetIcon( "icon16/time.png" )
-	
-		-- Acts
-		lrpDerma:AddSpacer()
-		if pl:IsFrozen() == false then
-			local FreezeUser = lrpDerma:AddOption( "Заморозить", function() net.Start( "freeze" ) net.WriteEntity(pl) net.SendToServer() end )
-			FreezeUser:SetIcon( "icon16/lock.png" )
-		else
-			local UnFreezeUser = lrpDerma:AddOption( "Разморозить", function() net.Start( "unfreeze" ) net.WriteEntity(pl) net.SendToServer() end )
-			UnFreezeUser:SetIcon( "icon16/lock_open.png" )
-		end
-		
-		if pl:IsOnFire() == false then
-			local IgniteUser, icon = lrpDerma:AddSubMenu("Поджечь")
-			icon:SetIcon( "icon16/weather_sun.png" )
-			IgniteUser:AddOption( "5 Секунд", function() net.Start( "5sec" ) net.WriteEntity(pl) net.SendToServer() end ):SetIcon( "icon16/time.png" )
-			IgniteUser:AddOption( "10 Секунд", function() net.Start( "10sec" ) net.WriteEntity(pl) net.SendToServer() end ):SetIcon( "icon16/time.png" )
-		else
-			local UnIgniteUser = lrpDerma:AddOption( "Потушить", function() net.Start( "unignite" ) net.WriteEntity(pl) net.SendToServer() end )
-			UnIgniteUser:SetIcon( "icon16/weather_rain.png" )
-		end
-		
-		local DamageUser, icon = lrpDerma:AddSubMenu("Установить здоровье")
-		icon:SetIcon( "icon16/heart_add.png" )
-		DamageUser:AddOption( "5 HP", function() net.Start( "5hp" ) net.WriteEntity(pl) net.SendToServer() end ):SetIcon( "icon16/heart.png" )
-		DamageUser:AddOption( "25 HP", function() net.Start( "25hp" ) net.WriteEntity(pl) net.SendToServer() end ):SetIcon( "icon16/heart.png" )
-		DamageUser:AddOption( "50 HP", function() net.Start( "50hp" ) net.WriteEntity(pl) net.SendToServer() end ):SetIcon( "icon16/heart.png" )
-		DamageUser:AddOption( "100 HP", function() net.Start( "100hp" ) net.WriteEntity(pl) net.SendToServer() end ):SetIcon( "icon16/heart.png" )
+    if LocalPlayer():IsAdmin() then
+        lrpDerma:AddSpacer()
 
-		local ArmorUser, icon = lrpDerma:AddSubMenu("Установить броню")
-		icon:SetIcon( "icon16/shield_add.png" )
-		ArmorUser:AddOption( "0 AR", function() net.Start( "0ar" ) net.WriteEntity(pl) net.SendToServer() end ):SetIcon( "icon16/shield.png" )
-		ArmorUser:AddOption( "25 AR", function() net.Start( "25ar" ) net.WriteEntity(pl) net.SendToServer() end ):SetIcon( "icon16/shield.png" )
-		ArmorUser:AddOption( "50 AR", function() net.Start( "50ar" ) net.WriteEntity(pl) net.SendToServer() end ):SetIcon( "icon16/shield.png" )
-		ArmorUser:AddOption( "100 AR", function() net.Start( "100ar" ) net.WriteEntity(pl) net.SendToServer() end ):SetIcon( "icon16/shield.png" )
+        addMenuOption(lrpDerma, "Кикнуть", "icon16/user_delete.png", function() 
+            net.Start("kickuser") 
+            net.WriteEntity(pl) 
+            net.SendToServer() 
+        end)
 
-		local DeathUser, icon = lrpDerma:AddSubMenu("Убить")
-		icon:SetIcon( "icon16/user_red.png" )
-		DeathUser:AddOption( "Убить", function() net.Start( "kill" ) net.WriteEntity(pl) net.SendToServer() end ):SetIcon( "icon16/heart_delete.png" )
-		DeathUser:AddOption( "Тихо убить", function() net.Start( "silkill" ) net.WriteEntity(pl) net.SendToServer() end ):SetIcon( "icon16/heart_delete.png" )
+        local banUser = addSubMenu(lrpDerma, "Забанить", "icon16/delete.png")
+        addMenuOption(banUser, "5 Минут", "icon16/time.png", function() 
+            net.Start("5m") 
+            net.WriteEntity(pl) 
+            net.SendToServer() 
+        end)
+        addMenuOption(banUser, "15 Минут", "icon16/time.png", function() 
+            net.Start("15m") 
+            net.WriteEntity(pl) 
+            net.SendToServer() 
+        end)
 
-		local RespawnUser = lrpDerma:AddOption( "Возродить", function() net.Start( "resp" ) net.WriteEntity(pl) net.SendToServer() end )
-		RespawnUser:SetIcon( "icon16/arrow_refresh.png" )
-	end
-	lrpDerma:Open()
+        lrpDerma:AddSpacer()
+
+        if not pl:IsFrozen() then
+            addMenuOption(lrpDerma, "Заморозить", "icon16/lock.png", function() 
+                net.Start("freeze") 
+                net.WriteEntity(pl) 
+                net.SendToServer() 
+            end)
+        else
+            addMenuOption(lrpDerma, "Разморозить", "icon16/lock_open.png", function() 
+                net.Start("unfreeze") 
+                net.WriteEntity(pl) 
+                net.SendToServer() 
+            end)
+        end
+
+        if not pl:IsOnFire() then
+            local igniteUser = addSubMenu(lrpDerma, "Поджечь", "icon16/weather_sun.png")
+            addMenuOption(igniteUser, "5 Секунд", "icon16/time.png", function() 
+                net.Start("5sec") 
+                net.WriteEntity(pl) 
+                net.SendToServer() 
+            end)
+            addMenuOption(igniteUser, "10 Секунд", "icon16/time.png", function() 
+                net.Start("10sec") 
+                net.WriteEntity(pl) 
+                net.SendToServer() 
+            end)
+        else
+            addMenuOption(lrpDerma, "Потушить", "icon16/weather_rain.png", function() 
+                net.Start("unignite") 
+                net.WriteEntity(pl) 
+                net.SendToServer() 
+            end)
+        end
+
+        local damageUser = addSubMenu(lrpDerma, "Установить здоровье", "icon16/heart_add.png")
+        for _, hp in ipairs({5, 25, 50, 100}) do
+            addMenuOption(damageUser, hp .. " HP", "icon16/heart.png", function() 
+                net.Start(tostring(hp) .. "hp") 
+                net.WriteEntity(pl) 
+                net.SendToServer() 
+            end)
+        end
+
+        local armorUser = addSubMenu(lrpDerma, "Установить броню", "icon16/shield_add.png")
+        for _, ar in ipairs({0, 25, 50, 100}) do
+            addMenuOption(armorUser, ar .. " AR", "icon16/shield.png", function() 
+                net.Start(tostring(ar) .. "ar") 
+                net.WriteEntity(pl) 
+                net.SendToServer() 
+            end)
+        end
+
+        local deathUser = addSubMenu(lrpDerma, "Убить", "icon16/user_red.png")
+        addMenuOption(deathUser, "Убить", "icon16/heart_delete.png", function() 
+            net.Start("kill") 
+            net.WriteEntity(pl) 
+            net.SendToServer() 
+        end)
+        addMenuOption(deathUser, "Тихо убить", "icon16/heart_delete.png", function() 
+            net.Start("silkill") 
+            net.WriteEntity(pl) 
+            net.SendToServer() 
+        end)
+
+        addMenuOption(lrpDerma, "Возродить", "icon16/arrow_refresh.png", function() 
+            net.Start("resp") 
+            net.WriteEntity(pl) 
+            net.SendToServer() 
+        end)
+    end
+
+    lrpDerma:Open()
 end
