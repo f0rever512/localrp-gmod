be8e53fea5799a8db95f91838a28efe69d633469
forever <maksimsidorov888@gmail.com>
2024-07-25 13:02:04 +0700

    :construction: Работа над интерфейсом и сохранением рейтинга
    :construction: Work on the interface and saving rating

--- lrp-scoreboard/lua/autorun/server/lrp_rating.lua
+++ lrp-scoreboard/lua/autorun/server/lrp_rating.lua
@@ -1,4 +1,23 @@
 util.AddNetworkString('sb-setRating')
+util.AddNetworkString('sb-clGetRating')
+util.AddNetworkString('sb-svGetRating')
+
+playerRating = playerRating or {}
+
+local function SavePlayerRating(data)
+    local jsonData = util.TableToJSON(data)
+    
+    file.Write('lrp_player_rating.txt', jsonData)
+end
+
+local function LoadPlayerRating()
+    if not file.Exists('lrp_player_rating.txt', "DATA") then return end
+
+    local jsonData = file.Read('lrp_player_rating.txt', "DATA")
+    playerRating = util.JSONToTable(jsonData) or {}
+end
+
+hook.Add("Initialize", 'LoadPlayerRating', LoadPlayerRating)
 
 Rating = Rating or {}
 net.Receive('sb-setRating', function(len, ply)
@@ -6,8 +25,16 @@ net.Receive('sb-setRating', function(len, ply)
     local target = net.ReadEntity()
     local bool = net.ReadBool()
 
-    if not Rating[target:SteamID()] then Rating[target:SteamID()] = 0 end
+    target:SetNWInt('rating', bool and target:GetNWInt('rating') + 1 or target:GetNWInt('rating') - 1)
+    playerRating[target:SteamID()] = tonumber(target:GetNWInt('rating'))
+    SavePlayerRating(playerRating)
+end)
 
-    Rating[target:SteamID()] = tonumber(bool and Rating[target:SteamID()] + 1 or Rating[target:SteamID()] - 1)
-    print(Rating[target:SteamID()])
+net.Receive('sb-clGetRating', function(len, ply)
+    if not ply:IsAdmin() then return end
+    local target = net.ReadEntity()
+    local rating = playerRating[target:SteamID()]
+    net.Start('sb-svGetRating')
+    net.WriteInt(rating, 12)
+    net.Send(ply)
 end)
