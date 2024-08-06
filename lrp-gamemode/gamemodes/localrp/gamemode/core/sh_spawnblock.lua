6cf901ade8ed157aa9fcc3a6cd58d0e5ca8cd4f9
forever <maksimsidorov888@gmail.com>
2024-08-06 15:31:29 +0700

    :ambulance: Фикс запрета на спавн-меню
    :ambulance: Fix spawnmenu block

--- lrp-gamemode/gamemodes/localrp/gamemode/core/sh_spawnblock.lua
+++ lrp-gamemode/gamemodes/localrp/gamemode/core/sh_spawnblock.lua
@@ -5,22 +5,24 @@ local whiteList = {
 
 local function canAccessSpawnMenu(ply)
     if not ply:Alive() then return false end
-    if ply:IsAdmin() or GetGlobalBool("AccessSpawn") then return true end
+    if GetGlobalBool("AccessSpawn") then return true end
 
     local wep = ply:GetActiveWeapon()
     return whiteList[wep:GetClass()] or false
 end
 
 if SERVER then
-    hook.Add("PlayerSpawnSWEP", "BlockSpawnMenu", function(ply, class)
-        return canAccessSpawnMenu(ply)
-    end)
+    local hooks = { 'PlayerSpawnVehicle', 'PlayerSpawnRagdoll', 'PlayerSpawnEffect', 'PlayerSpawnProp',
+    'PlayerSpawnSENT', 'PlayerSpawnNPC', 'PlayerSpawnSWEP', 'PlayerGiveSWEP', }
+    
+    for _, hookName in pairs(hooks) do
+        hook.Add(hookName, 'SpawnBlock', function(ply) return canAccessSpawnMenu(ply) end)
+    end
 else
-    local function spawnMenuBlock()
+    local function ClSpawnBlock()
         local ply = LocalPlayer()
         return canAccessSpawnMenu(ply)
     end
-
-    hook.Add("ContextMenuOpen", "hide_spawnmenu", spawnMenuBlock)
-    hook.Add("SpawnMenuOpen", "hide_spawnmenu", spawnMenuBlock)
+    hook.Add("ContextMenuOpen", 'SpawnBlock', ClSpawnBlock)
+    hook.Add("SpawnMenuOpen", 'SpawnBlock', ClSpawnBlock)
 end
