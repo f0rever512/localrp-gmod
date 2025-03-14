914e8a3abc48c03c6ecee977315bd5281fae38d0
forever <103198529+f0rever512@users.noreply.github.com>
2025-03-14 20:27:22 +0700

    :art: :lipstick: Улучшена структура кода и внешний вид информации над игроком
    :art: :lipstick: Improve code structure and overhead appearance

--- lrp-hud/lua/autorun/client/cl_overhead.lua
+++ lrp-hud/lua/autorun/client/cl_overhead.lua
@@ -2,71 +2,45 @@ CreateClientConVar("cl_lrp_overhead", "1", true, true)
 
 surface.CreateFont('lrpOverhead-font', {
     font = "Calibri",
-    size = 30,
+    size = 64,
     weight = 400,
     antialias = true,
     extended = true
 })
 
-hook.Add("PostPlayerDraw", "Overhead", function(ply)
+local width = 320
+local height = 24
+local posY = 32
+local offset = 4
+
+local voiceMat = Material('talkicon/voice.png')
+local textMat = Material('talkicon/text.png')
+
+hook.Add("PostPlayerDraw", 'lrpOverhead.draw', function(ply)
     if GetConVar('cl_lrp_overhead'):GetInt() < 1 then return end
-    if ply:GetPos():DistToSqr(EyePos()) > 160000 then return end
+    if ply:GetPos():DistToSqr(EyePos()) > 400 * 400 then return end
     if ply == LocalPlayer() and GetViewEntity() == LocalPlayer() and not ply:ShouldDrawLocalPlayer() then return end
     if not ply:Alive() or not IsValid(ply) or ply == LocalPlayer() then return end
     
-    local angle = EyeAngles()
+    local eyesAtt = ply:GetAttachment(ply:LookupAttachment('eyes'))
+    local pos = eyesAtt and eyesAtt.Pos + Vector(0, 0, 10) or ply:GetPos() + Vector(0, 0, ply:GetModelRadius() + 2)
+    local angle = Angle(0, EyeAngles().y, 0)
+    angle:RotateAroundAxis(angle:Up(), -90)
+    angle:RotateAroundAxis(angle:Forward(), 90)
 
-    angle = Angle( 0, angle.y, 0 )
-    angle:RotateAroundAxis( angle:Up(), -90 )
-    angle:RotateAroundAxis( angle:Forward(), 90 )
+    cam.Start3D2D( pos, angle, 0.05 )
+        draw.SimpleTextOutlined(ply:Nick(), 'lrpOverhead-font', 0, -posY, color_white, TEXT_ALIGN_CENTER, 0, 1, color_black )
 
-    local trace = LocalPlayer():GetEyeTrace()
-
-    local pos = ply:GetPos() + Vector(0, 0, ply:GetModelRadius() + 15)
-
-    local att = ply:GetAttachment(ply:LookupAttachment('eyes'))
-    if att then
-        pos = att.Pos + Vector(0, 0, 9)
-    end
-
-    cam.Start3D2D( pos, angle, 0.1 )
-        draw.SimpleTextOutlined(ply:Nick(), 'lrpOverhead-font', 0, -15, color_white, TEXT_ALIGN_CENTER, 0, 1, color_black )
-
-        local minus = -15
-        draw.RoundedBox(15, -76 , 0-minus, 150, 10, Color(22, 160, 133, 200), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-        if ply:Health() <= 100 then
-            draw.RoundedBox(15, -75, 1-minus, math.Clamp(ply:Health() or 0, 0, 180)*1.48, 8, Color(200, 0, 0, 225), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-            draw.RoundedBox(15, -75, 1-minus, math.Clamp(ply:Armor() or 0, 0, 180)*1.48, 8, Color(0, 70, 160, 215), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-        else
-            draw.RoundedBox(15, -75, 1-minus, 148, 8, Color(189, 0, 0,255))
-            draw.RoundedBox(15, -75, 1-minus, 148, 8, Color(0, 70, 160, 215))
-        end
+        draw.RoundedBox(10, width / -2, posY, width, height, Color(0, 185, 150, 200), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+        draw.RoundedBox(10, width / -2 + offset / 2, posY + offset / 2, math.Clamp(ply:Health() or 0, 0, 100) * (width - offset) / 100, height - offset, Color(200, 0, 0, 230), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+        draw.RoundedBox(10, width / -2 + offset / 2, posY + offset / 2, math.Clamp(ply:Armor() or 0, 0, 100) * (width - offset) / 100, height - offset, Color(0, 70, 160, 230), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
     cam.End3D2D()
-end)
 
-local voice_mat = Material('talkicon/voice.png')
-local text_mat = Material('talkicon/text.png')
-
-hook.Add('PostPlayerDraw', 'TalkIcon', function(ply)
-    if GetConVarNumber("cl_lrp_overhead") < 1 then return end
-    if ply:GetPos():DistToSqr(EyePos()) > 160000 then return end
-    if ply == LocalPlayer() and GetViewEntity() == LocalPlayer() and not ply:ShouldDrawLocalPlayer() then return end
-    if not ply:Alive() then return end
-    if !IsValid(ply) then return end
-    if ply == LocalPlayer() then return end
-
-    if not ply:IsSpeaking() and not ply:GetNW2Bool('ti_istyping') then return end
-
-    local pos = ply:GetPos() + Vector(0, 0, ply:GetModelRadius() + 15)
-
-    local attachment = ply:GetAttachment(ply:LookupAttachment('eyes'))
-    if attachment then
-        pos = ply:GetAttachment(ply:LookupAttachment('eyes')).Pos + Vector(0, 0, 13)
+    if ply:IsSpeaking() or ply:GetNW2Bool('ti_istyping') then
+        render.SetMaterial(ply:IsSpeaking() and voiceMat or textMat)
+        render.DrawSprite(pos + Vector(0, 0, 3), 4.2, 4.2, Color(0, 0, 0, 100))
+        render.DrawSprite(pos + Vector(0, 0, 3), 4, 4, Color(0, 185, 150, 240))
     end
-
-    render.SetMaterial(ply:IsSpeaking() and voice_mat or text_mat)
-
-    render.DrawSprite(pos, 5, 5, Color(22, 160, 133, 255))
 end)
 
 hook.Add('StartChat', 'TalkIcon', function(isteam)
