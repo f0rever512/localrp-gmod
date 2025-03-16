489911d32dcabc5c727ffe48cc1c31a6a5c883ba
forever <103198529+f0rever512@users.noreply.github.com>
2025-03-16 12:58:13 +0700

    :lipstick: Изменения информации над игроком
    :lipstick: Overhead changes

--- lrp-hud/lua/autorun/client/cl_overhead.lua
+++ lrp-hud/lua/autorun/client/cl_overhead.lua
@@ -24,23 +24,25 @@ hook.Add("PostPlayerDraw", 'lrpOverhead.draw', function(ply)
     
     local eyesAtt = ply:GetAttachment(ply:LookupAttachment('eyes'))
     local pos = eyesAtt and eyesAtt.Pos + Vector(0, 0, 10) or ply:GetPos() + Vector(0, 0, ply:GetModelRadius() + 2)
-    local angle = Angle(0, EyeAngles().y, 0)
+    local angle = Angle(EyeAngles().p, EyeAngles().y, 0)
     angle:RotateAroundAxis(angle:Up(), -90)
     angle:RotateAroundAxis(angle:Forward(), 90)
 
     cam.Start3D2D( pos, angle, 0.05 )
-        draw.SimpleTextOutlined(ply:Nick(), 'lrpOverhead-font', 0, -posY, color_white, TEXT_ALIGN_CENTER, 0, 1, color_black )
+        draw.SimpleTextOutlined(ply:Nick(), 'lrpOverhead-font', 0, posY - 64, color_white, TEXT_ALIGN_CENTER, 0, 1, color_black )
 
         draw.RoundedBox(10, width / -2, posY, width, height, Color(0, 185, 150, 200), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
         draw.RoundedBox(10, width / -2 + offset / 2, posY + offset / 2, math.Clamp(ply:Health() or 0, 0, 100) * (width - offset) / 100, height - offset, Color(200, 0, 0, 230), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
         draw.RoundedBox(10, width / -2 + offset / 2, posY + offset / 2, math.Clamp(ply:Armor() or 0, 0, 100) * (width - offset) / 100, height - offset, Color(0, 70, 160, 230), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+    
+        if ply:IsSpeaking() or ply:GetNW2Bool('ti_istyping') then
+            surface.SetMaterial(ply:IsSpeaking() and voiceMat or textMat)
+            surface.SetDrawColor(0, 0, 0, 100)
+            surface.DrawTexturedRect(-41, posY - 129, 82, 82)
+            surface.SetDrawColor(0, 185, 150, 240)
+            surface.DrawTexturedRect(-40, posY - 128, 80, 80)
+        end
     cam.End3D2D()
-
-    if ply:IsSpeaking() or ply:GetNW2Bool('ti_istyping') then
-        render.SetMaterial(ply:IsSpeaking() and voiceMat or textMat)
-        render.DrawSprite(pos + Vector(0, 0, 3), 4.2, 4.2, Color(0, 0, 0, 100))
-        render.DrawSprite(pos + Vector(0, 0, 3), 4, 4, Color(0, 185, 150, 240))
-    end
 end)
 
 hook.Add('StartChat', 'TalkIcon', function(isteam)
